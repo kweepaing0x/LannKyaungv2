7cf853a8fa29b2cdeb02f83ad39bf6c11b3e800d
kweepaing0x <166644326+kweepaing0x@users.noreply.github.com>
2026-05-13 08:15:58 +0700

    Update supabase.js

--- lann-kyaing-supabase/lann-kyaing-supa/src/supabase.js
+++ lann-kyaing-supabase/lann-kyaing-supa/src/supabase.js
@@ -5,7 +5,8 @@
 // ─────────────────────────────────────────────────────────────
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL  = "REPLACE_WITH_YOUR_PROJECT_URL";
-const SUPABASE_ANON = "REPLACE_WITH_YOUR_ANON_KEY";
+const SUPABASE_URL  = "https://ccrndognodytzqgsdwlb.supabase.co";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjcm5kb2dub2R5dHpxZ3Nkd2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTA0MjgsImV4cCI6MjA5NDE4NjQyOH0.1KafAemZ9RuUcROw41mrL6oIeGauArnkvSGT9dvHz2I";
+
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
